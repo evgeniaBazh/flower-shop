51513a2ccf097c871fb64dc396904bb9b466948d
Evgenia Bazhaikina <evgeniabazh7@gmail.com>
2022-08-16 10:50:35 +0500

    Добавлен раздел католог

--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -1,5 +1,30 @@
+import classes from "../catalog/Catalog.module.scss"
+import monobukety from '../../assets/monobukety.jpg'
+import sbornyeBukety from '../../assets/sbornye-bukety.jpg'
+import kompoziciiSKlubnikoi from '../../assets/kompozicii-s-klubnikoi.jpg'
+import { pathNames } from "../../App";
+import {NavLink} from "react-router-dom"
+
+
 function Catalog() {
-    return ( <h1>catalog</h1> );
+    return ( 
+    <div className="wrapper">
+        <div className="wrap">
+            <h1>Каталог</h1>
+            <div className={classes.block}>
+                <NavLink  to={pathNames.products}>
+                    <img className={classes.decor} src={monobukety} alt="Монобукеты" />
+                </NavLink>
+                <NavLink  to={pathNames.products}>
+                    <img className={classes.decor} src={sbornyeBukety} alt="Сборные букеты" />
+                </NavLink>
+                <NavLink  to={pathNames.products}>
+                    <img className={classes.decor} src={kompoziciiSKlubnikoi} alt="Композиции с клубникой" />
+                </NavLink>
+            </div>
+        </div>
+    </div>
+        );
 }
 
 export default Catalog;
