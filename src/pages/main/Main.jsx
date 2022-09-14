61ac35542b018a230d57dca9e5ce1dbdc5994cdd
Evgenia Bazhaikina <evgeniabazh7@gmail.com>
2022-09-14 13:33:01 +0500

    добавление шрифта и блока вопросы

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,13 +1,17 @@
+import ExpansionItem from "../../components/expansion-item/ExpansionItem";
+import Questions from "../../components/expansion-item/questions/Questions";
 import Catalog from "../catalog/Catalog";
 import Advantages from "./advantages/Advantages";
 import Carousel from "./carousel/Carousel";
 
 function Main() {
+    
     return ( 
         <div>
             <Carousel/>
             <Advantages/>
             <Catalog/>
+            <Questions/>
         </div>
     );
 }
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ExpansionItem from '../../components/expansion-item/ExpansionItem';
+import Questions from '../../components/expansion-item/questions/Questions';
 import Preloader from '../../components/Preloader';
 import { getProductById } from '../../services/api.service';
 import classes from '../product/Product.module.scss'
@@ -12,30 +13,7 @@ import Description from './description/Description';
 function Product(props) {
     const {id} = useParams();
     const [product, setProduct] = useState(null);
-    const accordionData = [
-        {
-          title: "Section 1",
-          content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-          laborum cupiditate possimus labore, hic temporibus velit dicta earum
-          suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-          voluptatem.`,
-        },
-        {
-          title: "Section 2",
-          content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-          reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-          quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-          pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-          quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-          Repudiandae, mollitia id reprehenderit a ab odit!`,
-        },
-        {
-          title: "Section 3",
-          content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-          quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-          dolor ut sequi minus iste? Quas?`,
-        },
-      ];
+    
     useEffect(() => {
         const getData = async () => {
             setProduct(await getProductById(id));
@@ -59,9 +37,7 @@ function Product(props) {
                     </div>
                 </div>
                 <Description/>
-                {accordionData.map(({ title, content }) => (
-                    <ExpansionItem title={title} key={title} content={content} />
-                ))}
+                <Questions/>
             </div>
             )
         } else {
